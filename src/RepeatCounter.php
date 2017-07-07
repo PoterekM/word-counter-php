998370f636df36a5364ae721535f909c2d0c8468
Michelle Poterek <poterekm@gmail.com>
2017-07-07 12:51:43 -0700

    able to account for most common punctuation by replacing punctuation with space

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,7 +5,12 @@
         {
             $input = strtolower($input);
             $input_string = strtolower($input_string);
+            $characters_to_split_on = array(' ', '.', ',', '!', '?', ';', ':', '#', '()', '&');
+            foreach($characters_to_split_on as $space){
+                $input_string = str_replace($space," ",$input_string);
+            }
             $split_input_string = explode(" ", $input_string);
+
 
             $count = 0;
             foreach ($split_input_string as $word) {
@@ -19,7 +24,6 @@
 
 
     }
-
 
 
 
