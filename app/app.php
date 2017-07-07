c7a7093325ab5790f215f021d7dde0a1b369e7bf
Michelle Poterek <poterekm@gmail.com>
2017-07-07 13:18:52 -0700

    form in views for user to input words and route it in app

--- app/app.php
+++ app/app.php
@@ -8,7 +8,9 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
-
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.html.twig');
+    });
 
     return $app;
 ?>
